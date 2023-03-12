d76ec5c81c778170f093a5f63cb5065adc3b0d26
jolan <jolanta.tubutyte@gmail.com>
2023-03-12 16:57:46 +0200

    Set up Driver

--- src/test/java/lt/jolanta/BTNproject/pom/utils/Driver.java
+++ src/test/java/lt/jolanta/BTNproject/pom/utils/Driver.java
@@ -1,4 +1,31 @@
 package lt.jolanta.BTNproject.pom.utils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class Driver {
+    public static ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--start-maximized ");
+        chromeOptions.addArguments("--force-device-scale-factor=0.5");
+        driverThread.set(new ChromeDriver(chromeOptions));
+        driverThread.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+}
+
+    public static WebDriver getDriver() {
+        return driverThread.get();
+    }
+
+    public static void closeDriver() {
+        driverThread.get().quit();
+        driverThread.remove();
+    }
 }
