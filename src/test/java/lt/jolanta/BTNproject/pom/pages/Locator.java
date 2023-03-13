9f9326a3fd9f0383dd7c696f8ac92f40bb237ca9
jolan <jolanta.tubutyte@gmail.com>
2023-03-13 17:24:27 +0200

    established locator mainSearch button

--- src/test/java/lt/jolanta/BTNproject/pom/pages/Locator.java
+++ src/test/java/lt/jolanta/BTNproject/pom/pages/Locator.java
@@ -1,4 +1,15 @@
 package lt.jolanta.BTNproject.pom.pages;
 
+import org.openqa.selenium.By;
+
 public class Locator {
+    public static By Elements = By.xpath("//button[@id='main_search_button']");
+
+    public class HomepageTest {
+        public static By inputUserMessage = By.xpath("//input[@id='main_search']");
+        public static By nameBannerTitle = By.xpath("//*[@id=body-inner]/div[1]/div/h1");
+        public static By mainSearchElement  = By.xpath("//button[@id='main_search_button']");
+        public static By autocompleteInput = By.xpath("//input[@class='ui-autocomplete-input']");
+        public static By nameMessage = By.xpath("//h1[@class='namebanner-title']");
+    }
 }
