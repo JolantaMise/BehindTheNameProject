af9d4f03b1341b52868639fc76372dff8ac5c1ae
jolan <jolanta.tubutyte@gmail.com>
2023-03-14 02:01:38 +0200

    deleted unused locator

--- src/test/java/lt/jolanta/BTNproject/pom/pages/Locator.java
+++ src/test/java/lt/jolanta/BTNproject/pom/pages/Locator.java
@@ -7,7 +7,6 @@ public class Locator {
 
     public class HomepageTest {
         public static By inputUserMessage = By.xpath("//input[@id='main_search']");
-        public static By nameBannerTitle = By.xpath("//*[@id=body-inner]/div[1]/div/h1");
         public static By mainSearchElement  = By.xpath("//button[@id='main_search_button']");
         public static By autocompleteInput = By.xpath("//input[@class='ui-autocomplete-input']");
         public static By nameMessage = By.xpath("//h1[@class='namebanner-title']");
