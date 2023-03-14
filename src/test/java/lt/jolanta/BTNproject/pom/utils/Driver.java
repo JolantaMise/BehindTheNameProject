85c84ec5cbdfba440c4281f7a1f2054b1447a89a
jolan <jolanta.tubutyte@gmail.com>
2023-03-14 22:38:11 +0200

    Added System.setProperty; chromeOptions setExperimentalOptions

--- src/test/java/lt/jolanta/BTNproject/pom/utils/Driver.java
+++ src/test/java/lt/jolanta/BTNproject/pom/utils/Driver.java
@@ -7,16 +7,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.Arrays;
 
 public class Driver {
     public static ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
 
     public static void setDriver() {
         WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        //chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         chromeOptions.addArguments("--start-maximized ");
-        //chromeOptions.addArguments("--force-device-scale-factor=0.5");
+        //chromeOptions.addArguments("--force-device-scale-factor=0.75");
         driverThread.set(new ChromeDriver(chromeOptions));
         driverThread.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
