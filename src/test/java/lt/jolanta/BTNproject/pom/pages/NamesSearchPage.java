a8eb9397b5454525c2826940229ea0105bbbd6e0
jolan <jolanta.tubutyte@gmail.com>
2023-03-14 10:52:21 +0200

    Created NamesSearchPage class

---
+++ src/test/java/lt/jolanta/BTNproject/pom/pages/NamesSearchPage.java
@@ -0,0 +1,8 @@
+package lt.jolanta.BTNproject.pom.pages;
+
+public class NamesSearchPage {
+
+    public static void open(){
+        Common.openUrl("https://www.behindthename.com/names/search");
+    }
+}
